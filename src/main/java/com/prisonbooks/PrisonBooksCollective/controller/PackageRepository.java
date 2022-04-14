e61fbeb7cbfca96b0f6285c808fcfe298d7ae876
Coal Cooper <cocowmn@gmail.com>
2022-04-13 23:56:24 -0400

    format

--- src/main/java/com/prisonbooks/PrisonBooksCollective/controller/PackageRepository.java
+++ src/main/java/com/prisonbooks/PrisonBooksCollective/controller/PackageRepository.java
@@ -9,7 +9,7 @@ import org.springframework.data.repository.CrudRepository;
 import java.time.LocalDate;
 import java.util.List;
 
-public interface PackageRepository extends CrudRepository<Package,Long> {
+public interface PackageRepository extends CrudRepository<Package, Long> {
 
     List<Package> findAllByDate(LocalDate date);
 
@@ -23,5 +23,7 @@ public interface PackageRepository extends CrudRepository<Package,Long> {
     List<Package> findAllByInmateNoId(long inmateId);
 
     List<Package> findAllByBooks(Book book);
+
     List<Package> findAllByNoISBNBooks(NoISBNBook book);
+
 }
